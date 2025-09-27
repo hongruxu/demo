212a046875d2869aec24e48d4ceff4541ab8f416
michaelxu <michaelxu@tencent.com>
2025-09-27 11:54:15 +0800

    feat: 部署脚本优化

--- copy_to_test.sh
+++ copy_to_test.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+
+# 生成 jar包
+./mvnw clean package
+
+# 上传到服务器
 scp ./target/demo-0.0.1-SNAPSHOT.jar ubuntu@114.132.58.71:/home/ubuntu/app/demo-0.0.1-SNAPSHOT.jar
+
+# 重启服务
+
+ssh ubuntu@114.132.58.71 '/bin/bash /home/ubuntu/app/restart.sh'
