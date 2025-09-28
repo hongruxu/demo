8cd5f42cd2613578c49b7190b010a97b9ddd0dee
michaelxu <michaelxu@tencent.com>
2025-09-28 15:42:18 +0800

    feat: 增加了prometheus数据输出并搭建了prometheus数据库及grafana看板

--- copy_to_test.sh
+++ copy_to_test.sh
@@ -4,8 +4,10 @@
 # 生成 jar包
 ./mvnw clean package
 
-# 上传到服务器
-cp ./restart.sh ./target/
+# 远程重启脚本放到目标目录
+cp ./script/restart.sh ./target/
+
+# jar包及脚本上传到服务器
 scp ./target/{demo-0.0.1-SNAPSHOT.jar,restart.sh} ubuntu@114.132.58.71:/home/ubuntu/app/
 
 # 重启服务
--- script/restart.sh
+++ script/restart.sh
@@ -2,4 +2,4 @@
 #测试机一个简单的重启
 PID=$(ps -ef|grep demo-0.0.1-SNAPSHOT.jar|grep -v grep |awk '{print $2}')
 kill $PID
-/usr/bin/java -jar /home/ubuntu/app/demo-0.0.1-SNAPSHOT.jar &
+nohup /usr/bin/java -jar /home/ubuntu/app/demo-0.0.1-SNAPSHOT.jar > /home/ubuntu/app/output.log 2>&1 &
